5d9acab9c3879ea8266f5b3288eae525a894c865
syl08 <syinlong08@gmail.com>
2022-07-09 23:00:07 +1000

    config axios

---
+++ src/utils/api/api.ts
@@ -0,0 +1,5 @@
+export const API = {
+  register: '/api/user/register',
+  login: '/api/auth/login',
+  getRate: '/api/rate/get/',
+};
---
+++ src/utils/api/index.ts
@@ -0,0 +1,50 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import qs from 'qs';
+
+axios.defaults.baseURL = 'https://cryptocurrency-rate-backend.herokuapp.com';
+
+axios.defaults.timeout = 10000;
+
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded';
+
+axios.defaults.transformRequest = (data) => qs.stringify(data);
+
+axios.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${token}`,
+      };
+      return config;
+    }
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (res: AxiosResponse) => {
+    return res;
+  },
+  (error: AxiosError) => {
+    const { response } = error;
+    if (response) {
+      switch (response.status) {
+        case 401:
+          localStorage.removeItem('token');
+          break;
+      }
+    } else {
+      if (!window.navigator.onLine) {
+        return;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
